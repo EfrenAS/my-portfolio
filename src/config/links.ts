9ab95a408cab4c698b39fd6faaac6aa5b41763e6
Efren Anastacio <50155831+EfrenAS@users.noreply.github.com>
2025-08-09 21:56:27 -0500

    [Updated] MenuMobile UI modified

---
+++ src/config/links.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+  { href: "#home", title: "Inicio", text: "Home" },
+  { href: "#projects", title: "Proyectos", text: "Proyectos" },
+  { href: "#work", title: "Experiencia", text: "Experiencia" },
+  { href: "#contact", title: "Contacto", text: "Contacto" },
+];
